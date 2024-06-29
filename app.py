424f27ad94bb8457f316f4fe6cc671d3923d5d39
Krish Patel <krishpatel5090@gmail.com>
2024-06-29 17:14:33 +0530

    Jinja added

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+def arrange_data(data):
+    result = {}
+    for item in data:
+        category = item.get('Category')
+        obj = item.get('Object')
+        image_link = item.get('Image link')
+
+        if category not in result:
+            result[category] = {}
+        
+        result[category][obj] = image_link
+    
+    return result
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    url = 'https://opensheet.elk.sh/15x9oFZtisE5Bl3s3pc-pJvWzsIKkjzQnFPtz9gMTra4/Sheet1'
+    response = requests.get(url)
+    data = response.json()
+    arranged_data = arrange_data(data)
+    return render_template('index.html', arranged_data=arranged_data)
 
 @app.route('/team')
 def team():
