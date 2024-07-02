c2267b486c72bd8b89772f3c148993eefbc42d42
Kunal Mishra <48254970+kunalm2345@users.noreply.github.com>
2024-07-02 23:06:06 +0530

    frontend ready (not for mobile yet tho)

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey'
 
 # FORMS
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField, PasswordField, TextAreaField, FileField, MultipleFileField
+from wtforms import RadioField, StringField, SubmitField, SelectField, PasswordField, TextAreaField, FileField, MultipleFileField
 from wtforms.validators import DataRequired,Email,EqualTo
 from wtforms import ValidationError, SelectMultipleField, widgets
 import email_validator
 
 class TargetForm(FlaskForm):
-    esp = SelectField('Which app do you send your newsletter?', choices=[('ml','MailerLite'), ('ck', 'Convertkit'), ('gh', 'Ghost'), ('rv', 'Revue'), ('eo', 'EmailOctopus'), ('sf', 'SendFox'), ('ac', 'ActiveCampaign'), ('br', 'Brevo (Sendinblue)'),], validators=[DataRequired()])
+    url = 'https://opensheet.elk.sh/15x9oFZtisE5Bl3s3pc-pJvWzsIKkjzQnFPtz9gMTra4/Sheet1'
+    response = requests.get(url)
+    data = response.json()
+    d=[]
+    for i in data:
+        d.append((i['Object'],i['Object']))
+
+    object = RadioField('Which object do you want to target?', choices=d, validators=[DataRequired()])
     apikey=StringField('Your API key', validators=[DataRequired()])
     listid=StringField('List ID', validators=[DataRequired()])
     submit = SubmitField('SAVE')
@@ -32,17 +40,21 @@ def arrange_data(data):
 
 @app.route('/')
 def home():
+    url = 'https://opensheet.elk.sh/15x9oFZtisE5Bl3s3pc-pJvWzsIKkjzQnFPtz9gMTra4/Sheet1'
+    response = requests.get(url)
+    data = response.json()
+    arranged_data = arrange_data(data)
     form = TargetForm()
     if form.validate_on_submit():
         print('Form submitted')
         print(form.esp.data)
         print(form.apikey.data)
         print(form.listid.data)
-    url = 'https://opensheet.elk.sh/15x9oFZtisE5Bl3s3pc-pJvWzsIKkjzQnFPtz9gMTra4/Sheet1'
-    response = requests.get(url)
-    data = response.json()
-    arranged_data = arrange_data(data)
     return render_template('index.html', form=form, arranged_data=arranged_data)
+
+@app.route('/send/', methods=['POST'])
+def send():
+    return request.form
 
 @app.route('/team/')
 def team():
