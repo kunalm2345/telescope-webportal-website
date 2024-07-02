cc116c6b5656327c9dda106d1b5b28dc68a4ab82
Kunal Mishra <48254970+kunalm2345@users.noreply.github.com>
2024-07-02 23:50:51 +0530

    minor fixes

--- app.py
+++ app.py
@@ -4,26 +4,6 @@ import requests
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
 
-# FORMS
-from flask_wtf import FlaskForm
-from wtforms import RadioField, StringField, SubmitField, SelectField, PasswordField, TextAreaField, FileField, MultipleFileField
-from wtforms.validators import DataRequired,Email,EqualTo
-from wtforms import ValidationError, SelectMultipleField, widgets
-import email_validator
-
-class TargetForm(FlaskForm):
-    url = 'https://opensheet.elk.sh/15x9oFZtisE5Bl3s3pc-pJvWzsIKkjzQnFPtz9gMTra4/Sheet1'
-    response = requests.get(url)
-    data = response.json()
-    d=[]
-    for i in data:
-        d.append((i['Object'],i['Object']))
-
-    object = RadioField('Which object do you want to target?', choices=d, validators=[DataRequired()])
-    apikey=StringField('Your API key', validators=[DataRequired()])
-    listid=StringField('List ID', validators=[DataRequired()])
-    submit = SubmitField('SAVE')
-
 def arrange_data(data):
     result = {}
     for item in data:
@@ -44,13 +24,7 @@ def home():
     response = requests.get(url)
     data = response.json()
     arranged_data = arrange_data(data)
-    form = TargetForm()
-    if form.validate_on_submit():
-        print('Form submitted')
-        print(form.esp.data)
-        print(form.apikey.data)
-        print(form.listid.data)
-    return render_template('index.html', form=form, arranged_data=arranged_data)
+    return render_template('index.html', arranged_data=arranged_data)
 
 @app.route('/send/', methods=['POST'])
 def send():
