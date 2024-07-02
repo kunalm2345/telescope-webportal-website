99277e655abe83e16b4d7182475129995c2c691b
Kunal Mishra <48254970+kunalm2345@users.noreply.github.com>
2024-07-02 17:16:29 +0530

    before forms

--- app.py
+++ app.py
@@ -3,6 +3,19 @@ import requests
 
 app = Flask(__name__)
 
+# FORMS
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField, PasswordField, TextAreaField, FileField, MultipleFileField
+from wtforms.validators import DataRequired,Email,EqualTo
+from wtforms import ValidationError, SelectMultipleField, widgets
+import email_validator
+
+class TargetForm(FlaskForm):
+    esp = SelectField('Which app do you send your newsletter?', choices=[('ml','MailerLite'), ('ck', 'Convertkit'), ('gh', 'Ghost'), ('rv', 'Revue'), ('eo', 'EmailOctopus'), ('sf', 'SendFox'), ('ac', 'ActiveCampaign'), ('br', 'Brevo (Sendinblue)'),], validators=[DataRequired()])
+    apikey=StringField('Your API key', validators=[DataRequired()])
+    listid=StringField('List ID', validators=[DataRequired()])
+    submit = SubmitField('SAVE')
+
 def arrange_data(data):
     result = {}
     for item in data:
@@ -19,13 +32,19 @@ def arrange_data(data):
 
 @app.route('/')
 def home():
+    form = TargetForm()
+    if form.validate_on_submit():
+        print('Form submitted')
+        print(form.esp.data)
+        print(form.apikey.data)
+        print(form.listid.data)
     url = 'https://opensheet.elk.sh/15x9oFZtisE5Bl3s3pc-pJvWzsIKkjzQnFPtz9gMTra4/Sheet1'
     response = requests.get(url)
     data = response.json()
     arranged_data = arrange_data(data)
-    return render_template('index.html', arranged_data=arranged_data)
+    return render_template('index.html', form=form, arranged_data=arranged_data)
 
-@app.route('/team')
+@app.route('/team/')
 def team():
     return render_template('team.html')
 
